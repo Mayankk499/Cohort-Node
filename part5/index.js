07ac4a0a2d7d6900e757f0d08f4d9dd26755be26
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-09-18 12:30:35 +0530

    refactor: enhance book management API with error handling and validation

--- part5/index.js
+++ part5/index.js
@@ -1,17 +1,70 @@
-import express from 'express'
+import express from "express";
 const app = express();
 
-app.get('/', function(req, res) {
-    res.end("Homepage");
+const PORT = 8000;
+
+const books = [
+  { id: 1, title: "Book One", author: "John Capler" },
+  { id: 2, title: "Book Two", author: "John Cena" },
+];
+
+app.use(express.json());
+
+app.get("/books", (req, res) => {
+  res.json(books);
 });
 
-app.get('/contact-us', function(req, res){
-    res.end('you can contact on mk@mk.com');
+app.get("/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id))
+    return res.status(400).json({ error: `id must be of type number` });
+  const Book = books.find((e) => e.id === id);
+
+  if (!Book)
+    return res
+      .status(404)
+      .json({ error: `Book with id ${id} does not exists` });
+
+  return res.json(Book);
 });
 
-app.post('/tweet', function(req, res){
-    res.status(200)
-    .end('tweet created success');
+app.post("/books", (req, res) => {
+  const { title, auther } = req.body;
+
+  if (!title || title === "")
+    return res.status(400).json({ error: "title is required 📝" });
+
+  if (!auther || auther === "")
+    return res.status(400).json({ error: "auther is required 👨‍🏫" });
+
+  const id = books.length + 1;
+
+  const book = { id, title, auther };
+  books.push(book);
+
+  return res.status(201).json({ message: "Book Added Successfully ✅" });
 });
 
-app.listen(8000, () => console.log(`Server is listening on port 8000`));
+app.delete("/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "id must be of type number" });
+  }
+
+  const indexToDelete = books.findIndex((e) => e.id === id);
+
+  if (indexToDelete < 0) {
+    return res.status(404).json({ error: `Book with id ${id} does not exist 🚫` });
+  }
+
+  books.splice(indexToDelete, 1);
+
+  return res.status(200).json({ message: "Book removed ✅" });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port${PORT}`);
+});
