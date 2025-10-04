6ac1ca92fd58d634e205a1e7f93124e8810ed07b
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-10-04 22:34:04 +0530

    feat: refactor user routes for improved readability and structure

--- mongoDB/routes/user.routes.js
+++ mongoDB/routes/user.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
-import  User  from "../models/user.model.js";
+import User from "../models/user.model.js";
 import { randomBytes, createHmac } from "crypto";
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
 router.post("/signup", async (req, res) => {
@@ -26,7 +27,37 @@ router.post("/signup", async (req, res) => {
     salt,
   });
 
-  return res.status(201).json({status: 'success', data: {id: user._id} });
+  return res.status(201).json({ status: "success", data: { id: user._id } });
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({
+    email,
+  });
+
+  if (!existingUser) {
+    return res.status(404).json({ error: `User Not found` });
+  }
+
+  const salt = existingUser.salt;
+  const hashed = existingUser.password;
+
+  const newHash = createHmac("sha256", salt).update(password).digest("hex");
+
+  if (hashed !== newHash) {
+    return res.status(400).json({ error: `Incorrect password` });
+  }
+
+  const payload = {
+      _id: existingUser._id,
+    name: existingUser.name,
+    email: existingUser.email,
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
+  return res.json({status: 'success', token});
 });
 
 export default router;
